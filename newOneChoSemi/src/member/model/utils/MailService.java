d0fa66c81a7a64613c2ea7c282bc3ac48e61cedb
Helkanes <kangkwangsan@gmail.com>
2020-06-18 16:33:52 +0900

    Gmail로 마스터 계정 추가... gmail 설정 법 추가

--- newOneChoSemi/src/member/model/utils/MailService.java
+++ newOneChoSemi/src/member/model/utils/MailService.java
@@ -14,12 +14,17 @@ import javax.mail.internet.MimeMessage;
 
 public class MailService {
 
-   private final String USER = "";   // 운영자의 이메일 아이디
-   private final String PASSWORD = "";      // 운영자의 이메일 비밀번호
+   private final String USER = "onebunonecho@gmail.com";   // 운영자의 이메일 아이디
+   private final String PASSWORD = "Onecho1234";      // 운영자의 이메일 비밀번호
    
    /*
   		1. Properties -  Java Build Path - Libraries - Add JARs - javax.mail 추가
-		2. 네이버 내 메일함 톱니바퀴 - 메일함 관리 - IMAP/SMTP - 사용함 체크
+		
+		-> 네이버 메일
+		2.1) 네이버 내 메일함 톱니바퀴 - 메일함 관리 - IMAP/SMTP - 사용함 체크
+		-> gmail 설정 법
+		2.2) gmail 설정 - > 전달 및 POP/IMAP -> IMAP 액세스에서 IMAP 사용 체크
+		
 		3. 전역변수 USER랑 PASSWORD : 운영자의 네이버 로그인 (이메일/비번)
 		(테스트할때만 맞춰놓고 지워두거나 바꿔두기 ==> 깃허브 조심!!)
     */
@@ -34,9 +39,12 @@ public class MailService {
    public void sendMail(String email, String pwd) throws Exception{
       // 이메일 사용을 위해 필요한 속성값들
       Properties prop = new Properties();
-      prop.put("mail.smtp.host", "smtp.naver.com"); // 구글일 경우 smtp.google.com
-      prop.put("mail.smtp.port", 587);           	// 구글일 경우 465
+//      prop.put("mail.smtp.host", "smtp.naver.com"); // 구글일 경우 smtp.google.com
+//      prop.put("mail.smtp.port", 587);           	// 네이버일 경우 465
+      prop.put("mail.smtp.host", "smtp.gmail.com"); // 구글일 경우 smtp.google.com
+      prop.put("mail.smtp.port", 465);           	// 구글일 경우 465
       prop.put("mail.smtp.auth", "true");
+      prop.put("mail.smtp.ssl.enable", "true");		// 구글은 이거 추가해야함
       
       // SSL 사용하고 있을 경우 선택할 수 있는 옵션 인듯 하다
 //      prop.put("mail.smtp.ssl.enable", "true");
