0caaff11f8d87e9e55cbed05fcaa76068e5cba83
Helkanes <kangkwangsan@gmail.com>
2020-06-19 16:57:36 +0900

    방문자 카운트용 테이블 추가

---
+++ newOneChoSemi/src/sql/script/visit.sql
@@ -0,0 +1,56 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 금요일-6월-19-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table VISIT
+--------------------------------------------------------
+
+  CREATE TABLE "VISIT" 
+   (	"V_DATE" DATE
+   )
+REM INSERTING into VISIT
+SET DEFINE OFF;
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/06','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+Insert into VISIT (V_DATE) values (to_date('20/06/19','RR/MM/DD'));
+--------------------------------------------------------
+--  Constraints for Table VISIT
+--------------------------------------------------------
+
+  ALTER TABLE "VISIT" MODIFY ("V_DATE" NOT NULL ENABLE)
